5db324f349cf78d89075926b37b4e1383e4e5e88
Guido de Hek <guidodehek@hotmail.nl>
2021-10-02 16:58:44 +0200

    feat: add electricalengineering stylesheet

--- ipe-7.2.24/RUN_IPE.bat
+++ ipe-7.2.24/RUN_IPE.bat
@@ -1,3 +1,3 @@
 cd bin
-start ipe.exe -sheet ..\styles\symbolsettings -sheet ..\styles\blockdiagram -sheet ..\styles\electromechanical -sheet ..\styles\components -sheet ..\styles\converters -sheet ..\styles\filtercircuits
+start ipe.exe -sheet ..\styles\symbolsettings -sheet ..\styles\blockdiagram -sheet ..\styles\electromechanical -sheet ..\styles\components -sheet ..\styles\converters -sheet ..\styles\filtercircuits -sheet ..\styles\ElectricalEngineering
 exit
