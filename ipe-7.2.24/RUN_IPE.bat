16c29de3f08dc02a311c81f9cdc18924b3a5ed31
Guido de Hek <guidodehek@hotmail.nl>
2021-11-13 11:03:57 +0100

    feat: add single line library
    feat: add small fuse

--- ipe-7.2.24/RUN_IPE.bat
+++ ipe-7.2.24/RUN_IPE.bat
@@ -1,3 +1,3 @@
 cd bin
-start ipe.exe -sheet ..\styles\symbolsettings -sheet ..\styles\blockdiagram -sheet ..\styles\electromechanical -sheet ..\styles\components -sheet ..\styles\converters -sheet ..\styles\filtercircuits -sheet ..\styles\ElectricalEngineering
+start ipe.exe -sheet ..\styles\symbolsettings -sheet ..\styles\blockdiagram -sheet ..\styles\electromechanical -sheet ..\styles\components -sheet ..\styles\converters -sheet ..\styles\filtercircuits -sheet ..\styles\ElectricalEngineering -sheet ..\styles\singleline
 exit
