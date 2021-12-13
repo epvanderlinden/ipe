22cedc1bacf33ecdf55508e2c4958b775d7315a1
Guido de Hek <guido.de.hek@LWH.Local>
2021-12-13 10:53:17 +0100

    feat: library for control systems

--- ipe-7.2.24/RUN_IPE.bat
+++ ipe-7.2.24/RUN_IPE.bat
@@ -1,3 +1,3 @@
 cd bin
-start ipe.exe -sheet ..\styles\symbolsettings -sheet ..\styles\colors -sheet ..\styles\a4paper -sheet ..\styles\blockdiagram -sheet ..\styles\Semiconductors -sheet ..\styles\converters -sheet ..\styles\filtercircuits -sheet ..\styles\ElectricalEngineering -sheet ..\styles\singleline
+start ipe.exe -sheet ..\styles\symbolsettings -sheet ..\styles\colors -sheet ..\styles\a4paper -sheet ..\styles\ControlSystems -sheet ..\styles\blockdiagram -sheet ..\styles\Semiconductors -sheet ..\styles\converters -sheet ..\styles\filtercircuits -sheet ..\styles\ElectricalEngineering -sheet ..\styles\singleline
 exit
